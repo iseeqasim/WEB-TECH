6186f112f407193e7492e55eaea6815eb8847669
Muhammad Qasim <iseeqasim@gmail.com>
2023-06-01 18:30:53 +0500

    CRUD
    CRUD Operations Added

--- Assignment3+4/my-movie-ticketing-app/models/Ticket.js
+++ Assignment3+4/my-movie-ticketing-app/models/Ticket.js
@@ -23,4 +23,24 @@ const ticketSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('Ticket', ticketSchema);
+const Ticket = mongoose.model('Ticket', ticketSchema);
+
+// Create a new ticket
+const newTicket = new Ticket({
+  movie: 'Avengers: Endgame',
+  cinema: 'Cineplex',
+  date: new Date('2023-06-01'),
+  time: '18:00',
+  tickets: 2
+});
+
+// Save the ticket to the database
+newTicket.save()
+  .then(savedTicket => {
+    console.log('Ticket saved:', savedTicket);
+  })
+  .catch(error => {
+    console.error('Error saving ticket:', error);
+  });
+
+module.exports = Ticket;
--- Assignment3+4/my-movie-ticketing-app/server.js
+++ Assignment3+4/my-movie-ticketing-app/server.js
@@ -21,6 +21,92 @@ mongoose.connect('mongodb://127.0.0.1:27017/latestdb', {
     console.error('MongoDB connection error:', err);
   });
 
+// Import the Ticket model
+const Ticket = require('./models/Ticket');
+
+// Create a new ticket
+app.post('/tickets', (req, res) => {
+  const { movie, cinema, date, time, tickets } = req.body;
+  const newTicket = new Ticket({
+    movie,
+    cinema,
+    date,
+    time,
+    tickets
+  });
+
+  newTicket.save()
+    .then(savedTicket => {
+      res.status(201).json(savedTicket);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error saving ticket' });
+    });
+});
+
+// Retrieve all tickets
+app.get('/tickets', (req, res) => {
+  Ticket.find()
+    .then(tickets => {
+      res.json(tickets);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error retrieving tickets' });
+    });
+});
+
+// Retrieve a single ticket by ID
+app.get('/tickets/:id', (req, res) => {
+  const { id } = req.params;
+
+  Ticket.findById(id)
+    .then(ticket => {
+      if (ticket) {
+        res.json(ticket);
+      } else {
+        res.status(404).json({ error: 'Ticket not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error retrieving ticket' });
+    });
+});
+
+// Update a ticket by ID
+app.put('/tickets/:id', (req, res) => {
+  const { id } = req.params;
+  const { movie, cinema, date, time, tickets } = req.body;
+
+  Ticket.findByIdAndUpdate(id, { movie, cinema, date, time, tickets }, { new: true })
+    .then(updatedTicket => {
+      if (updatedTicket) {
+        res.json(updatedTicket);
+      } else {
+        res.status(404).json({ error: 'Ticket not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error updating ticket' });
+    });
+});
+
+// Delete a ticket by ID
+app.delete('/tickets/:id', (req, res) => {
+  const { id } = req.params;
+
+  Ticket.findByIdAndDelete(id)
+    .then(deletedTicket => {
+      if (deletedTicket) {
+        res.json({ message: 'Ticket deleted successfully' });
+      } else {
+        res.status(404).json({ error: 'Ticket not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error deleting ticket' });
+    });
+});
+
 // Define routes here
 const homeRouter = require('./routes/Home');
 const quickTicketsRouter = require('./routes/QT');
@@ -28,7 +114,8 @@ const contactRouter = require('./routes/Contact');
 const signInRouter = require('./routes/Signin');
 const signUpRouter = require('./routes/Signup');
 
-app.use('/Home', homeRouter);
+app.use('/', homeRouter);
+app.use('/home', homeRouter);
 app.use('/QT', quickTicketsRouter);
 app.use('/Contact', contactRouter);
 app.use('/Signin', signInRouter);

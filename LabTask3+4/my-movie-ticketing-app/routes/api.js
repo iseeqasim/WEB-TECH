94ffde7a663895a08364179a6195bb07f2a6600d
Muhammad Qasim <iseeqasim@gmail.com>
2023-06-08 17:50:44 +0500

    Assignments Folders
    Folders

---
+++ LabTask3+4/my-movie-ticketing-app/routes/api.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const Ticket = require('../models/Ticket');
+
+
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+
+//Create a new ticket
+router.post('/tickets', (req, res) => {
+  const { ticketId, movie, cinema, date, time, tickets } = req.body;
+  const newTicket = new Ticket({
+    ticketId,
+    movie,
+    cinema,
+    date,
+    time,
+    tickets
+  });
+
+
+  newTicket.save()
+    .then(savedTicket => {
+      res.redirect('/map'); // Redirect to the map.ejs page //PRG
+      // const response = {
+      //   message: 'Ticket added successfully',
+      //   ticket: savedTicket
+      // };
+      // const formattedResponse = JSON.stringify(response, null, 2);
+      // res.setHeader('Content-Type', 'application/json');
+      // res.send(formattedResponse);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error saving ticket' });
+    });
+});
+
+
+// Retrieve all tickets
+router.get('/tickets', (req, res) => {
+  Ticket.find()
+    .then(tickets => {
+      res.render('map', { tickets: tickets }); 
+      // const response = {
+      //   ticket: tickets
+      // };
+      // const formattedResponse = JSON.stringify(response, null, 1);
+      // res.setHeader('Content-Type', 'application/json');
+      // res.send(formattedResponse);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error retrieving tickets' });
+    });
+});
+
+// Retrieve single ticket
+router.post('/tickets/id', (req, res) => {
+  const { ticketId } = req.body;
+  
+  Ticket.findOne({ ticketId: ticketId }) // Use 'ticketId' in the query
+    .then(ticket => {
+      if (ticket) {
+        res.render('map', { tickets: [ticket] }); 
+        // const response = {
+        //   ticket: ticket
+        // };
+        // const formattedResponse = JSON.stringify(response, null, 1);
+        // res.setHeader('Content-Type', 'application/json');
+        // res.send(formattedResponse);
+      } else {
+        res.status(404).json({ error: 'Ticket not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error retrieving ticket' });
+    });
+});
+
+
+
+
+// Update a ticket by ID
+router.post('/tickets/upd', (req, res) => {
+  const { ticketId } = req.body;
+  const { time, tickets } = req.body;
+
+  Ticket.findOneAndUpdate({ ticketId: ticketId }, { time, tickets }, { new: true })
+    .then(updatedTicket => {
+      if (updatedTicket) {
+        const response = {
+          message: 'Ticket updated successfully',
+          ticket: updatedTicket
+        };
+        const formattedResponse = JSON.stringify(response, null, 2);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(formattedResponse);
+      } else {
+        res.status(404).json({ error: 'Ticket not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error updating ticket' });
+    });
+});
+
+
+
+
+// Delete a ticket by ID
+router.post('/tickets/del', (req, res) => {
+  const { ticketId } = req.body;
+
+  Ticket.findOneAndDelete({ ticketId: ticketId })
+    .then(deletedTicket => {
+      if (deletedTicket) {
+        res.json({ message: 'Ticket deleted successfully' });
+      } else {
+        res.status(404).json({ error: 'Ticket not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error deleting ticket' });
+    });
+});
+
+module.exports = router;
---
+++ LabTask3+4/my-movie-ticketing-app/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Ticket = require('./models/Ticket');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+// Serve static files from the 'public' folder
+app.use(express.static('public'));
+
+//PRG
+app.get('/map', (req, res) => {
+  Ticket.find()
+    .then(tickets => {
+      res.render('map', { tickets: tickets });
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error retrieving tickets' });
+    });
+});
+
+
+
+
+// MongoDB connection
+mongoose.connect('mongodb://127.0.0.1:27017/Cinetick', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
+
+
+// Define routes here
+const homeRouter = require('./routes/Home');
+const quickTicketsRouter = require('./routes/QT');
+const contactRouter = require('./routes/Contact');
+const signInRouter = require('./routes/Signin');
+const signUpRouter = require('./routes/Signup');
+const TicketsUpRouter = require('./routes/TicketsManage');
+const apiRouter = require('./routes/api');
+
+app.use('/', homeRouter);
+app.use('/home', homeRouter);
+app.use('/QT', quickTicketsRouter);
+app.use('/Contact', contactRouter);
+app.use('/Signin', signInRouter);
+app.use('/Signup', signUpRouter);
+app.use('/TicketsManage', TicketsUpRouter);
+app.use('/api', apiRouter);
+
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
